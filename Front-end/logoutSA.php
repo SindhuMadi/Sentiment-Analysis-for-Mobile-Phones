967d5c50786f6144e60536399b5303dba295a8bb
SindhuMadi <42187175+SindhuMadi@users.noreply.github.com>
2021-06-15 11:29:55 -0400

    Update logoutSA.php
    Changed loginSA.php to index.php

--- Front-end/logoutSA.php
+++ Front-end/logoutSA.php
@@ -3,5 +3,5 @@
     session_unset();
     #unset($_SESSION["login_user"]);  // where $_SESSION["nome"] is your own variable. if you do not have one use only this as follow **session_unset();**
     session_destroy();
-    header("Location: loginSA.php");
+    header("Location: index.php");
 ?>
